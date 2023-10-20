c03bc568e28024acd2f798b7e6e07e4365d62d3a
Vishal Singh <rvishal2710@gmail.com>
2023-10-21 01:06:08 +0530

    footer
    updating

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,22 +3,180 @@ import "./index.css";
 
 const Footer = () => {
   return (
-    <div className="main-news-footer-container">
-      <div className="footer-news-text-news">
-        <div className="footer-news-text">
-          <h4>Sign up for our newsletter!</h4>
-          <p>Get notified about launch, updates and weekly blogs.</p>
-        </div>
-        <div className="footer-news-subscribe">
-          <input
-            className="input-news"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <button className="news-button">Subscribe</button>
+    <>
+      <div className="main-news-footer-container">
+        <div className="footer-news-text-news">
+          <div className="footer-news-text">
+            <h4>Sign up for our newsletter!</h4>
+            <p>Get notified about launch, updates and weekly blogs.</p>
+          </div>
+          <div className="footer-news-subscribe">
+            <input
+              className="input-news"
+              type="text"
+              placeholder="Enter your email"
+            />
+            <button className="news-button">
+              <a href="">
+                <span>Subscribe</span>
+              </a>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div>
+        <div className="middle-section-footer">
+          <div>
+            <div className="company-logo-details">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="37"
+                height="36"
+                viewBox="0 0 37 36"
+                fill="none"
+              >
+                <g clip-path="url(#clip0_1003_491)">
+                  <path
+                    d="M3.43502 24.4195H0.725098V34.4948C0.725098 35.0702 1.19204 35.5371 1.76738 35.5371H13.5104V34.4948C13.5104 28.9395 8.99037 24.4195 3.43502 24.4195Z"
+                    fill="url(#paint0_angular_1003_491)"
+                  />
+                  <path
+                    d="M10.731 9.07642C7.58469 9.5774 5.17216 12.3096 5.17216 15.5948V22.4586C7.23656 22.7553 9.1356 23.5731 10.731 24.7731V9.07642Z"
+                    fill="url(#paint1_angular_1003_491)"
+                  />
+                  <path
+                    d="M24.0722 21.4399V0.0996094H20.6674C17.478 0.0996094 14.634 1.61022 12.8155 3.95396V21.4448C15.8878 18.1649 21.058 18.2059 24.0722 21.4399Z"
+                    fill="url(#paint2_angular_1003_491)"
+                  />
+                  <path
+                    d="M26.1567 4.59461V26.9793C27.2909 26.7169 27.0912 26.782 36.1626 26.782V15.5948C36.1626 9.83221 31.7618 5.11707 26.1567 4.59461Z"
+                    fill="url(#paint3_angular_1003_491)"
+                  />
+                  <path
+                    d="M22.2655 34.4948V35.5371H35.1203C35.6957 35.5371 36.1626 35.0702 36.1626 34.4948V28.8665H27.8938C24.7906 28.8665 22.2655 31.3916 22.2655 34.4948Z"
+                    fill="url(#paint4_angular_1003_491)"
+                  />
+                  <path
+                    d="M24.0722 27.7971C24.0722 26.4653 24.2291 24.537 22.424 22.7331C18.8883 19.1951 12.8155 21.7323 12.8155 26.7125C12.8155 26.9109 15.595 29.6212 15.595 34.4948V35.5371H20.181V34.4948C20.181 31.6313 21.7493 29.1278 24.0722 27.7971Z"
+                    fill="url(#paint5_angular_1003_491)"
+                  />
+                </g>
+                <defs>
+                  <radialGradient
+                    id="paint0_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <radialGradient
+                    id="paint1_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <radialGradient
+                    id="paint2_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <radialGradient
+                    id="paint3_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <radialGradient
+                    id="paint4_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <radialGradient
+                    id="paint5_angular_1003_491"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(18.4438 17.8184) rotate(120) scale(17.7188)"
+                  >
+                    <stop stop-color="#0538FF" />
+                    <stop offset="1" stop-color="#5799F7" />
+                  </radialGradient>
+                  <clipPath id="clip0_1003_491">
+                    <rect
+                      width="35.4375"
+                      height="35.4375"
+                      fill="white"
+                      transform="translate(0.725342 0.0996094)"
+                    />
+                  </clipPath>
+                </defs>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="117"
+                height="32"
+                viewBox="0 0 117 32"
+                fill="none"
+              >
+                <path
+                  d="M0.267578 24.8775V8.04473H3.45695V24.8775H0.267578ZM11.643 24.8775V14.707C11.643 13.4549 11.2532 12.4744 10.4736 11.7657C9.71758 11.0333 8.76077 10.6671 7.60314 10.6671C6.82352 10.6671 6.12658 10.8325 5.51233 11.1632C4.89808 11.494 4.40195 11.9547 4.02395 12.5453C3.64595 13.1359 3.45695 13.8447 3.45695 14.6715L2.14577 14.0337C2.14577 12.7579 2.41745 11.6475 2.96083 10.7025C3.52783 9.75754 4.29564 9.02517 5.26427 8.50542C6.23289 7.96204 7.31964 7.69035 8.52452 7.69035C9.70576 7.69035 10.7689 7.95023 11.7139 8.46998C12.6825 8.98973 13.4385 9.7221 13.9819 10.6671C14.5489 11.6121 14.8324 12.7343 14.8324 14.0337V24.8775H11.643ZM23.0184 24.8775V14.707C23.0184 13.4549 22.6286 12.4744 21.849 11.7657C21.093 11.0333 20.148 10.6671 19.014 10.6671C18.2344 10.6671 17.5256 10.8325 16.8878 11.1632C16.2735 11.494 15.7774 11.9547 15.3994 12.5453C15.0214 13.1359 14.8324 13.8447 14.8324 14.6715L13.0251 14.0337C13.096 12.7343 13.4385 11.6239 14.0528 10.7025C14.6906 9.75754 15.5057 9.02517 16.498 8.50542C17.4902 7.96204 18.577 7.69035 19.7582 7.69035C20.9867 7.69035 22.0853 7.95023 23.0539 8.46998C24.0225 8.98973 24.7903 9.7221 25.3573 10.6671C25.948 11.6121 26.2433 12.7461 26.2433 14.0691V24.8775H23.0184Z"
+                  fill="#373737"
+                />
+                <path
+                  d="M36.1115 25.0193L28.8114 8.04473H32.3551L37.7771 21.3338H36.6431L42.3485 8.04473H45.8922L38.0251 25.0193H36.1115ZM31.292 31.9296L36.2532 21.759L38.0251 25.0193L34.8357 31.9296H31.292Z"
+                  fill="#373737"
+                />
+                <path
+                  d="M51.9404 24.8775V21.1566H58.4609C60.1619 21.1566 61.6503 20.7904 62.9261 20.058C64.2254 19.3257 65.2295 18.3098 65.9382 17.0104C66.6706 15.6874 67.0368 14.1636 67.0368 12.439C67.0368 10.738 66.6706 9.23779 65.9382 7.93842C65.2059 6.63904 64.19 5.62316 62.8906 4.89079C61.6149 4.15841 60.1383 3.79223 58.4609 3.79223H51.9404V0.0712891H58.5672C60.3627 0.0712891 62.0283 0.390227 63.5639 1.0281C65.0995 1.64235 66.4344 2.51648 67.5684 3.65048C68.726 4.76085 69.6237 6.07204 70.2616 7.58404C70.8995 9.07242 71.2184 10.7025 71.2184 12.4744C71.2184 14.2227 70.8995 15.8528 70.2616 17.3648C69.6237 18.8768 68.7378 20.1998 67.6038 21.3338C66.4698 22.4442 65.135 23.3183 63.5994 23.9562C62.0637 24.5704 60.41 24.8775 58.6381 24.8775H51.9404ZM49.1409 24.8775V0.0712891H53.2162V24.8775H49.1409Z"
+                  fill="#373737"
+                />
+                <path
+                  d="M75.3729 24.8775V0.0712891H79.4482V24.8775H75.3729ZM78.1725 24.8775V21.1566H92.064V24.8775H78.1725ZM78.1725 13.9982V10.419H90.8591V13.9982H78.1725ZM78.1725 3.79223V0.0712891H91.8868V3.79223H78.1725Z"
+                  fill="#373737"
+                />
+                <path
+                  d="M96.2755 24.8775V0.0712891H99.1105L100.351 4.74904V24.8775H96.2755ZM113.463 24.8775L98.6144 5.49323L99.1105 0.0712891L113.959 19.4556L113.463 24.8775ZM113.463 24.8775L112.222 20.5187V0.0712891H116.298V24.8775H113.463Z"
+                  fill="#373737"
+                />
+              </svg>
+              <p>Revolutionize apartment living with our feature-rich management app.</p>
+            </div>
+            <div className="products"></div>
+            <div className="quick-link"></div>
+            <div className="legal"></div>
+            <div className="get-app"></div>
+          </div>
+          <div className="last-section-footer"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
